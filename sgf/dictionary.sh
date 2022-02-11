639829123e0ea7beb186052b902e3f013653706e
okonomichiyaki <okonomichiyaki@gmail.com>
2022-02-11 12:38:55 -0500

    scraper for new OJE

--- sgf/dictionary.sh
+++ sgf/dictionary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ruby main.rb $DEPTH dictionary.sgf $NODES root
+ruby main.rb $DEPTH dictionary.sgf $NODES 15081
 tr -d "[:space:]" < dictionary.sgf > dictionary2.sgf 
 mv dictionary2.sgf dictionary.sgf 
