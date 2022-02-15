3446a4152c07fc82c032730a3a5e787c943fc7d0
Ye Wang <yewang15@gmail.com>
2022-02-15 13:03:01 -0500

    Scroll to make OJE link visible

--- app.js
+++ app.js
@@ -141,7 +141,6 @@ function toggleButtons() {
         makeButton("Share", "Copy results to clipboard without spoiler tags", share)
     );
     document.querySelector('#Submit').classList.add("hidden");
-    scrollHints();
 }
 function showExplorerLink(nodeId) {
     if (nodeId === null || nodeId === undefined) { // old puzzles don't have node
@@ -262,6 +261,7 @@ function submit() {
     if (solved) {
         toggleButtons();
         showExplorerLink(puzzle.node_id);
+        scrollHints();
     }
 }
 
